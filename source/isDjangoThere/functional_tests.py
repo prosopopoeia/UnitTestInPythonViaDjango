8abde3c5bf1050ee7cb6ee1ddd456bf61d313a0b
Solomon Grundy <ngwtt@yahoo.com>
2019-08-07 14:28:59 -0600

    more clean up

--- source/isDjangoThere/functional_tests.py
+++ source/isDjangoThere/functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def tearDown(self):
         self.browser.quit()
         
@@ -24,23 +29,34 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('ta\' da', header_text)
         
-        ##s/he is invited to make one, and does so
-        inputbox = self.browser.find_element_by_id('id_new_item')
         # self.assertEqual(
-            # inputbox.get_attribute('placeholder'),
-            # 'Enter a to-do item'
+        #    inputbox.get_attribute('placeholder'),
+        #    'Enter a to-do item'
         # )
         
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy Item" as an item in a to-do list table
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy Item')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy Item')
         
-        #page updates automatically as items are added
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy Item', [row.text for row in rows])
-        elf.assertIn('2: Use Item', [row.text for row in rows])
-        ##Kippers sees another text box
+        # There is still a text box inviting her to add another item. She
+        # enters "Use Item"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use Item')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy Item')
+        self.check_for_row_in_list_table('2: Use Item')
+        
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        
+        ##Kippers wonders if site remembers list - sees another text box
         self.fail('finis')
         ##another update
         
