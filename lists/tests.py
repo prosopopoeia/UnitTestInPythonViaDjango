508ddf13e3d8420cb13da44ee025e9dc54020467
Solomon Grundy <ngwtt@yahoo.com>
2019-08-08 21:25:49 -0600

    more notes, documented stuff in settings, new unit test, updated model

--- lists/tests.py
+++ lists/tests.py
@@ -43,10 +43,14 @@ class ItemModelTest(TestCase):
         first_item.text = 'The first (ever) list item'
         first_item.save()
         
-        second_item = Item.objects.all()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
         
-        first_save_item = saved_items[0]
+        first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')        
