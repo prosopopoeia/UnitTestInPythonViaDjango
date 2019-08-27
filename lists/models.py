39fa39aa91581b457be67dacbcd05957581ee36f
Solomon Grundy <ngwtt@yahoo.com>
2019-08-26 19:28:03 -0600

    got rid of superfluous and aligned tests

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+
+
+class List(models.Model):
+    pass
+    # list = models.TextField(default='')
+     
+    # def save(self):
+        # models.save()
+        
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item  ##import ITEM object found in view file -> added code
+from lists.models import Item, List  ##import ITEM object found in view file -> added code
 
 ########################################################################################################
 ##the Django test runner automatically creates a brand new test database (separate from the real one),##
@@ -16,34 +16,26 @@ class HomePageTest(TestCase):
         # found = resolve('/')
         # self.assertEqual(found.func, home_page)
         
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
+    # def test_home_page_returns_correct_html(self):
+        # response = self.client.get('/')
+        # self.assertTemplateUsed(response, 'home.html')
         
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
         
-    def test_can_save_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+    # def test_can_save_POST_request(self):
+        # response = self.client.post('/', data={'item_text': 'A new list item'})
         
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
+        # self.assertEqual(Item.objects.count(), 1)
+        # new_item = Item.objects.first()
+        # self.assertEqual(new_item.text, 'A new list item')
         
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+    # def test_redirects_after_POST(self):
+        # response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        # self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
         
-    # def test_displays_all_list_items(self):
-        # Item.objects.create(text='itemey 1')
-        # Item.objects.create(text='itemey 2')
-        
-        # response = self.client.get('/')
-        
-        # self.assertIn('itemey 1', response.content.decode())
-        # self.assertIn('itemey 2', response.content.decode())
         ##########the code below is replaced by Django supplied test framework###########
         # request = HttpRequest()
         # response = home_page(request)
@@ -86,24 +78,34 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
  
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
-        
+
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
-        
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
-        
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')        
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
      
         
         
